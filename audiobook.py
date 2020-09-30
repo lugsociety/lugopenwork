6e9d7921b81071fe9afd33bc0aa9717a834b365c
lugsociety <71563611+lugsociety@users.noreply.github.com>
2020-10-01 01:12:49 +0530

    uploaded

---
+++ audiobook.py
@@ -0,0 +1,12 @@
+import pyttsx3
+import PyPDF2
+book = open("C:\\Users\\hp\\Desktop\\401853005_RaghavWadhwa.pdf", 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+
+speaker = pyttsx3.init()
+for num in range(1, pages):
+    page = pdfReader.getPage(num)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
